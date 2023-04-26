3141408d4c593640cb4178108733dca82ac0b9cd
PradheshKumar <pradheshkumar29@gmail.com>
2023-04-26 20:23:37 +0530

    Fixed Home URl

--- app.js
+++ app.js
@@ -124,7 +124,7 @@ app.use((req, res, next) => {
 });
 
 // 3) ROUTES
-// app.use("/", viewRouter);
+app.use("/", incidentRouter);
 app.use("/api/v1/incidents", incidentRouter);
 // app.use("/api/v1/user", buyerRouter);
 // app.use("/api/v1/seller", sellerRouter);
